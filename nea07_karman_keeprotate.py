ef36e9f2248858d7778d9ffdbc2c7861e7436167
Xu Yuyang <167343312+xyy2024@users.noreply.github.com>
2025-08-31 11:18:53 +0800

    Add files via upload

--- nea07_karman_keeprotate.py
+++ nea07_karman_keeprotate.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#    Test code for Numerical Experiment Appendix 07
+#    Copyright (C) 2025 Xu Yuyang (https://github.com/xyy2024)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+
 from d2n5_karman import d2n5_karman, np, d2n5_poiseuille
 
 class model(d2n5_karman):
--- neb01_lid_driven_swsqr.py
+++ neb01_lid_driven_swsqr.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#    Test code for Numerical Experiment Appendix 01
+#    Copyright (C) 2025 Xu Yuyang (https://github.com/xyy2024)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+
 from _plottools import prt_flow_2d, gridfig, prt_mask_2d, save, show
 from d2n5_lid_driven import d2n5_lid_driven, np
 
