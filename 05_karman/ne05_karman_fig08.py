d5a190817385bd539b56bdd27380e8648711727b
Xu Yuyang <167343312+xyy2024@users.noreply.github.com>
2025-10-10 12:10:07 +0800

    Update ne05_karman_fig08.py

--- 05_karman/ne05_karman_fig08.py
+++ 05_karman/ne05_karman_fig08.py
@@ -18,9 +18,36 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # 
 
-from d2n5_karman import d2n5_karman
+from d2n5_karman import d2n5_karman, gridfig, prt_2d, cmap, prt_mask_2d
 
 test = d2n5_karman(h = 1/32, nu = 0.002)
 test.save.prefix = "karman"
 test.save.dir = "graphic\\Karman\\00_default"
 test.animation(time = 100, time_step = None, fps = 10, file_name = f"animation.mp4")
+
+'''
+Because I've changed $u, v$ in the paper to $u_1, u_2$, the xlabel of the result graphic should also be changed. 
+
+In order to change the xlabel, I should run:
+
+`
+def setting(self):
+    u_num, v_num = self.get_numerical_speed()
+    fig, axs = gridfig(2, 1)
+    fig.set_figheight(8)
+    fig.set_figwidth(20)
+
+    prt_2d(self.x, self.y, u_num, fig=fig, ax=axs[0], xlabel="$u_1$", cmap = cmap)
+    prt_2d(self.x, self.y, v_num, fig=fig, ax=axs[1], xlabel="$u_2$", cmap = cmap, norm = self.vnorm)
+
+    for ax in axs:
+        prt_mask_2d(ax, (self.xmin, self.xmax), (self.ymin, self.ymax), lambda x, y: self.border_func(x, y))
+    
+    fig.tight_layout()
+    return fig, axs
+
+test.animation(time = 100, time_step = None, fps = 10, file_name = f"animation.mp4", setting=setting)
+`
+
+But run this will cost a lot of time. So I won't run it again only for changing the xlabel.
+'''
